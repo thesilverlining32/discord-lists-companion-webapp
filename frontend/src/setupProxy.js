eaa194940a8617bc4093409d0633f684cd128e4f
Garrett Giordano <garrett@apexceph.com>
2024-05-15 23:55:07 -0700

    frontend: src: setupProxy: add /auth rout
    This definition will allow the /auth to run on a different port if
    desired.
    
    Signed-off-by: Garrett Giordano <garrett@apexceph.com>

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:3000',
-            changeOrigin: true,
-        })
-    );
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/auth',
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+    })
+  );
 };
