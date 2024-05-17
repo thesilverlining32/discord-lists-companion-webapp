50e0ff7989c8fbf426607f33674776e9cf8c61bd
Garrett Giordano <garrett@apexceph.com>
2024-05-17 05:32:16 -0700

    Update package.json
    Signed-off-by: Garrett Giordano <garrett@apexceph.com>

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import List from './List';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/" component={List} />
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<List />} />
+      </Routes>
     </Router>
   );
 }
