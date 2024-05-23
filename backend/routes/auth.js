8e1d11b1f3e23305389dddb0b51c1a304f1e15df
Garrett Giordano <garrett@apexceph.com>
2024-05-23 01:23:44 -0700

    fix logout error
    Signed-off-by: Garrett Giordano <garrett@apexceph.com>

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const passport = require('passport');
-const User = require('../models/User');
-
 const router = express.Router();
 
-// Discord Authentication Routes
+// Authentication routes
 router.get('/auth/discord', passport.authenticate('discord'));
-router.get('/auth/discord/callback', passport.authenticate('discord', {
-    failureRedirect: '/',
-    successRedirect: '/'
-}));
+
+router.get(
+    '/auth/discord/callback',
+    passport.authenticate('discord', {
+        failureRedirect: '/',
+        successRedirect: '/',
+    })
+);
 
 router.get('/auth/status', (req, res) => {
     if (req.isAuthenticated()) {
         res.json({ user: req.user });
     } else {
-        res.status(401).json({ user: null });
+        res.json({ user: null });
     }
 });
 
-router.get('/auth/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+router.get('/auth/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
