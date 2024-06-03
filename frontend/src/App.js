f1dd48a1b7ee161553aa1fc5b0fdd473ac28d4b2
Garrett Giordano <ggior32@gmail.com>
2024-06-02 17:09:19 -0700

    fixup correct discord callback handling
    Signed-off-by: Garrett Giordano <ggior32@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,16 +24,12 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-                    <Route path="/list/:id" element={<PrivateRoute><List /></PrivateRoute>} />
+                    <Route path="/dashboard" element={isAuthenticated() ? <Dashboard /> : <Navigate to="/login" />} />
+                    <Route path="/list/:id" element={isAuthenticated() ? <List /> : <Navigate to="/login" />} />
                 </Routes>
             </div>
         </Router>
     );
 }
-
-const PrivateRoute = ({ children }) => {
-    return isAuthenticated() ? children : <Navigate to="/login" />;
-};
 
 export default App;
