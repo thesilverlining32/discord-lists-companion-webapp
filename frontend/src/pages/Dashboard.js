c2f5a78e52653ab5fe60fe6aee273c07fbe194c2
Garrett Giordano <ggior32@gmail.com>
2024-08-07 22:38:20 -0700

    Add missing pages and footer
    Signed-off-by: Garrett Giordano <ggior32@gmail.com>

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Your lists and ideas will appear here.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Idea List</h1>
+      <p>Create and manage lists of ideas with your friends!</p>
+    </div>
+  );
+};
+
+export default Home;
