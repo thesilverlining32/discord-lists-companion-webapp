52feee95b6fec84d90dbcbed8996fdc6f1ecac51
Garrett Giordano <ggior32@gmail.com>
2024-10-09 23:23:42 -0700

    use env frontend and api for origins
    Signed-off-by: Garrett Giordano <ggior32@gmail.com>

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from typing import List
+from urllib.parse import urlparse
 
 class Settings(BaseSettings):
     mongodb_url: str
@@ -12,7 +13,6 @@ class Settings(BaseSettings):
     omdb_api_key: str
     react_app_api_url: str
     react_app_frontend_url: str
-    cors_origins: List[str] = [react_app_api_url, react_app_frontend_url]
 
     class Config:
         env_file = "../.env"
@@ -20,5 +20,14 @@ class Settings(BaseSettings):
     @property
     def mongodb_connection_string(self):
         return f"mongodb://{self.mongo_initdb_root_username}:{self.mongo_initdb_root_password}@{self.mongodb_url}"
+
+    @property
+    def cors_origins(self) -> List[str]:
+        origins = [self.react_app_frontend_url]
+        api_origin = urlparse(self.react_app_api_url).netloc
+        if api_origin not in origins:
+            origins.append(f"http://{api_origin}")
+            origins.append(f"https://{api_origin}")
+        return origins
 
 settings = Settings()
