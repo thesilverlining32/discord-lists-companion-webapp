e5668de274192709b01ae25072771b16ec8ca66c
Garrett Giordano <ggior32@gmail.com>
2025-03-29 17:34:51 -0700

    fixup models.py
    Signed-off-by: Garrett Giordano <ggior32@gmail.com>

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 from typing import Optional, Dict, Any, List, Literal
+from enum import Enum
 from pydantic import BaseModel, Field, ConfigDict
 from pydantic_core import core_schema
 
